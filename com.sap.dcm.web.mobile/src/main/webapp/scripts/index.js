843e224421bc3c7bc0de623704e4670f8326bbcd
Wang, Chengchang <chengchang.wang@sap.com>
2011-10-17 10:49:02 +0800

    t
    :q
    
    Change-Id: I61e5a7d63180b110bd63f3e22dfcb280bcf342b3

---
+++ com.sap.dcm.web.mobile/src/main/webapp/scripts/index.js
@@ -0,0 +1,10 @@
+V5.registerPage("index", function () {
+    var initialize = function () {
+        var page = this;
+        var view = new V5.View(page.node);
+        
+    };
+    return {
+        initialize: initialize
+    };
+});
