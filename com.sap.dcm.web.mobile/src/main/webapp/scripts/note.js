81d6d43ef5ae1ad6f8559818d6cc2025f95b423d
Wang, Chengchang <chengchang.wang@sap.com>
2011-10-20 17:41:55 +0800

    Add note page and related resources(including data, script, css,template).
    Change-Id: I06477b0099612a68d8e8747f31011a2933e0a855

--- com.sap.dcm.web.mobile/src/main/webapp/scripts/note.js
+++ com.sap.dcm.web.mobile/src/main/webapp/scripts/note.js
@@ -5,6 +5,41 @@ App.registerPage("note", function () {
         var proxy = new EventProxy();
         
         
+        console.log(noteView.$("article"));
+        
+        //fetch the notes' data
+        noteView.fetchData = function (url) {
+            $.getJSON("ajax/"+ url +".json", function (data) {
+               proxy.trigger("data", data);
+            });
+        };
+        noteView.fetchData("notes");
+        
+        //fetch the template for notes
+        App.getTemplate("note", function (template) {
+            proxy.trigger("template", template);
+        });
+        
+        proxy.assign("template", "data", function (template, data) {
+           var html = _.template(template, {notes: data.results});
+           noteView.$(".notes_set").html(html);
+           var iscroll = new iScroll(noteView.$(".notes_set")[0], {
+               useTransform : false,
+               onBeforeScrollStart : function (e) {
+                   var target = e.target;
+                   while (target.nodeType !== 1) {
+                       target = target.parentNode;
+                   }
+                   if (target.tagName !== 'SELECT' && target.tagName !== 'INPUT'
+                           && target.tagName !== 'TEXTAREA') {
+                       e.preventDefault();
+                   }
+               },
+               scrollbarClass : 'myScrollbar'
+           });
+        });
+        
+        
     };
     return {
         initialize: initialize
