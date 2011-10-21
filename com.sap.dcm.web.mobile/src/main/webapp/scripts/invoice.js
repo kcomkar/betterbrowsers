fc636220b0c01499c6e1c367239b0c6d694ea764
Jackson Tian <shyvo1987@gmail.com>
2011-10-21 15:27:55 -0700

    Add invoice detail page.
    Change-Id: Ibbc32ce4cdaac962dde6d1b3f84f0fad12640959

---
+++ com.sap.dcm.web.mobile/src/main/webapp/scripts/invoice.js
@@ -0,0 +1,33 @@
+App.registerPage("invoice", function () {
+    var initialize = function () {
+        var page = this;
+        var scroll;
+        var view = new App.View(page.node);
+        var proxy = new EventProxy();
+
+        proxy.assign("data", "template", function (data, template) {
+            var html = _.template(template, data);
+            view.$(".detail").html(html);
+            scroll = new iScroll(view.$("article")[0], {
+            onScrollEnd: function () {
+                
+                }
+            });
+        });
+
+        App.getTemplate("invoice_detail", function (tmpl) {
+            proxy.trigger("template", tmpl);
+        });
+
+        // TODO
+        // $.getJSON("invoice_detail", function (data) {
+            // proxy.trigger("data", data);
+        // });
+        
+        proxy.trigger("data", {});
+    };
+
+    return {
+        initialize: initialize
+    }
+});
