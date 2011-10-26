3b33235a863b6e3e6dbe1c3d7ed1de5eee55053a
Wang, Chengchang <chengchang.wang@sap.com>
2011-10-26 16:09:10 +0800

    Add hitlist.js to merge the conflict.
    Change-Id: Ia285d69b337127cfe95002537a6c1981fbc5eded

--- com.sap.dcm.web.mobile/src/main/webapp/scripts/hitlist.js
+++ com.sap.dcm.web.mobile/src/main/webapp/scripts/hitlist.js
@@ -1,16 +1,25 @@
 App.registerPage("hitlist", function () {
+
     var initialize = function () {
         var page = this;
         var listView = new App.View(page.node);
         var proxy = new EventProxy();
         
         //fetch data from the given url
-        listView.fetchData = function (url) {
-            $.getJSON("ajax/"+ url +".json", function (data) {
-                proxy.trigger("data", data.collectionOverviewResponse);
+        listView.fetchData = function () {
+            $.ajax({
+                type:'GET',
+                url:'rest/mobile/collectionOverview/getOverview',
+                dataType:'json',
+                success:function (data) {
+                    proxy.trigger("data", data.collectionOverviewResponse);
+                },
+                error: function (msg) {
+                    proxy.trigger("error", msg);
+                }
             });
         };
-        listView.fetchData("hitlist");
+        listView.fetchData();
         //fetch hitlist.tmpl here
         App.getTemplate("hitlist", function (template) {
             proxy.trigger("template:hitlist", template);
@@ -34,12 +43,16 @@ App.registerPage("hitlist", function () {
             var navTabView = new AppUI.TabView(tabviewNode, modules);
         });
         
+        proxy.assign("error", function (msg) {
+            listView.$(".collection").html("Eror occurs!");
+        });
         
         listView.bind("showDetail", function (event) {
             var target = $(event.currentTarget);
             var itemId = target.data("id");
             console.log("show detail--id=" + itemId);
-            page.openView("detail/" + itemId, false);
+            var itemCode = target.data("code");
+            page.openView("detail/" + itemId + "/" + itemCode, false);
         });
         
         listView.delegateEvents({
@@ -47,7 +60,14 @@ App.registerPage("hitlist", function () {
         });
         
     };
+    
+    var reappear = function () {
+        var page = this;
+        page.initialize();
+    };
+    
     return {
-        initialize: initialize
+        initialize: initialize,
+        reapear: reappear
     };
 });
